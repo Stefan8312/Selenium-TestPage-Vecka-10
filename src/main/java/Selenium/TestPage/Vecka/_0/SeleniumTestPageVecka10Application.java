44cd6397dd0b590f8296fddee334b94f8c4671de
Stefan8312 <stefand16@hotmail.com>
2026-04-25 23:37:22 +0200

    Initial commit

---
+++ src/main/java/Selenium/TestPage/Vecka/_0/SeleniumTestPageVecka10Application.java
@@ -0,0 +1,12 @@
+package Selenium.TestPage.Vecka._0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumTestPageVecka10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeleniumTestPageVecka10Application.class, args);
+    }
+}
---
+++ src/test/java/Selenium/TestPage/Vecka/_0/SeleniumTestExample.java
@@ -0,0 +1,90 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class SeleniumTestExample {
+
+    @Test
+    public void seleniumTest() {
+
+        // Automatically downloads ChromeDriver
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.kimmoahola.net/selenium.html");
+
+        // 1
+        WebElement title =
+                driver.findElement(By.id("main-title"));
+        System.out.println("Title: " + title.getText());
+
+        // 2
+        WebElement paragraph =
+                driver.findElement(By.className("text-paragraph"));
+        System.out.println("Paragraph: " + paragraph.getText());
+
+        // 3
+        WebElement username =
+                driver.findElement(By.name("username"));
+        username.sendKeys("TestUser");
+
+        // 4
+        WebElement checkbox =
+                driver.findElement(By.name("check2"));
+
+        if (!checkbox.isSelected()) {
+            checkbox.click();
+        }
+
+        // 5
+        WebElement radioBlue =
+                driver.findElement(By.cssSelector("input[value='blue']"));
+        radioBlue.click();
+
+        // 6
+        WebElement dropdownElement =
+                driver.findElement(By.id("dropdown"));
+
+        Select dropdown =
+                new Select(dropdownElement);
+
+        dropdown.selectByVisibleText("Option 3");
+
+        // 7
+        WebElement button =
+                driver.findElement(By.className("btn-class"));
+        button.click();
+
+        // 8
+        WebElement link =
+                driver.findElement(By.id("link-id"));
+
+        String href =
+                link.getAttribute("href");
+
+        System.out.println("Link href: " + href);
+
+        // 9
+        WebElement nestedSpan =
+                driver.findElement(By.cssSelector("#nested-span"));
+
+        System.out.println("Nested span: "
+                + nestedSpan.getText());
+
+        // 10
+        WebElement image =
+                driver.findElement(By.id("image-id"));
+
+        String altText =
+                image.getAttribute("alt");
+
+        System.out.println("Image alt: " + altText);
+
+        driver.quit();
+    }
+}
